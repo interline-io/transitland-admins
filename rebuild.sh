bdd6ff6885018169049ef7f159222f0507ae6aae
Ian Rees <ian@ianrees.net>
2025-02-05 18:32:35 -0800

    Simplify rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-go install cmd/transitland-admins
-transitland-admins -idkey tzid zipgeojson https://github.com/evansiroky/timezone-boundary-builder/releases/download/2024a/timezones-now.geojson.zip timezones/timezones.polyline
-transitland-admins -idkey name -include name,iso_a2,iso_3166_2,admin shapefile https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_1_states_provinces.zip admins/admins.polyline
+go run cmd/transitland-admins/main.go -idkey tzid zipgeojson https://github.com/evansiroky/timezone-boundary-builder/releases/download/2024a/timezones-now.geojson.zip timezones/timezones.polyline
+go run cmd/transitland-admins/main.go -idkey name -include name,iso_a2,iso_3166_2,admin shapefile https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_1_states_provinces.zip admins/admins.polyline
