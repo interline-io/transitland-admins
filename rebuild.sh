116f6a29f7166dd994c1394c850772d92fbdee27
Ian Rees <ian@ianrees.net>
2025-02-06 01:25:11 -0800

    Move command to transitland-lib

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-(cd cmd/transitland-admins && go install .)
-transitland-admins -idkey tzid zipgeojson https://github.com/evansiroky/timezone-boundary-builder/releases/download/2024a/timezones-now.geojson.zip timezones/timezones.polyline
-transitland-admins -idkey name -include name,iso_a2,iso_3166_2,admin shapefile https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_1_states_provinces.zip admins/admins.polyline
+transitland polylines-create --idkey tzid zipgeojson "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2024a/timezones-now.geojson.zip" "timezones/timezones.polyline"
+transitland polylines-create --idkey name -k name -k iso_a2 -k iso_3166_2 -k admin shapefile "https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_1_states_provinces.zip" "admins/admins.polyline"
